3f79f76e4db78bbd15ca98da1851aa3c98014072
iam-dionsuby <iam.dionsuby@gmail.com>
2025-07-15 22:54:18 +0530

    Delete dion directory

--- dion/day_4.py
+++
@@ -1,7 +0,0 @@
-string=input("Enter the string")
-vowel="AEIOUaeiou"
-count=0
-for i in range (len(string)):
-        if string[i] in vowel:
-                count+=1
-print("Number of vowels = ",count)      
